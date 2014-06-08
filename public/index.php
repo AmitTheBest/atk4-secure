f73588cd580e5631b21d84ca293f91c1be5ca3a7
Romans Malinovskis <romans@agiletoolkit.org>
2014-06-08 16:38:13 +0100

    fix

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ if(function_exists('xdebug_enable')){
     xdebug_enable();
 }
 chdir('..');
-require_once'../vendor/autoload.php';
+require_once'vendor/autoload.php';
 require_once 'lib/Frontend.php';
 $api=new Frontend('front');
 $api->main();
